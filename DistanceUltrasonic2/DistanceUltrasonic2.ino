b797015ce4e881109abbf5a8c813b2c0bee6de82
York <yukuan.jiang@gmail.com>
2016-08-18 23:36:27 +0800

    Added and verified the 4-bit LED Digital Tube Module.

---
+++ DistanceUltrasonic2/DistanceUltrasonic2.ino
@@ -0,0 +1,34 @@
+/**
+ * @file DistanceUltrasonic2.ino
+ *  This program shows an ultrasonic measured distance on the 4 digit 7
+ *  segment LED display.
+ *
+ * @author Jiang Yu-Kuan <yukuan.jiang@gmail.com>
+ * @date 2016/08/18 (initial version)
+ * @date 2016/08/18 (last revision)
+ * @version 1.0
+ */
+
+#define USE_PULSE_MODE
+
+typedef enum {
+    PIN_SCLK = 10,
+    PIN_RCLK = 11,
+    PIN_DIO = 12
+} Pin;
+
+
+void setup()
+{
+    LED4_init(PIN_SCLK, PIN_RCLK, PIN_DIO);
+}
+
+
+void loop()
+{
+    uint16_t len_mm = 1234;
+
+    LED4_step(len_mm);
+}
+
+
