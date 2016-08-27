25e0d7fcbe00363749012fae8a83db3632286da9
York <yukuan.jiang@gmail.com>
2016-08-27 16:50:21 +0800

    Changed output unit form mm to cm.

--- DistanceUltrasonic2/DistanceUltrasonic2.ino
+++ DistanceUltrasonic2/DistanceUltrasonic2.ino
@@ -5,7 +5,7 @@
  *
  * @author Jiang Yu-Kuan <yukuan.jiang@gmail.com>
  * @date 2016/08/18 (initial version)
- * @date 2016/08/19 (last revision)
+ * @date 2016/08/27 (last revision)
  * @version 1.0
  */
 
@@ -23,24 +23,34 @@ void setup()
 {
     HCSR04_init(PIN_Trig, PIN_Echo);
     Digits_init(PIN_SCLK, PIN_RCLK, PIN_DIO);
+    Serial.begin(19200);
 }
 
 
 void loop()
 {
-    uint16_t len_mm;
+    enum {
+        REPEAT_PERIOD = 2000 // in milliseconds
+    };
+    static unsigned long endMillis = 0;
+    static float len_cm;
 
-    if (!HCSR04_measure(&len_mm) || !HCSR04_isValidDistance(len_mm)) {
-        unsigned long endMillis = millis() + 1000;
-        Digits_clear();
-        while (millis() < endMillis)
-            ;
-        return;
+    if (millis() >= endMillis) {
+        endMillis = millis() + REPEAT_PERIOD;
+
+        uint16_t len_mm = 0;
+
+        if (!HCSR04_measure(&len_mm))
+            len_cm = NAN;   // error
+        else if (!HCSR04_isValidDistance(len_mm))
+            len_cm = 10000; // out of range
+        else
+            len_cm = (float)len_mm/10.;
+
+        Serial.println(len_cm);
     }
 
-    unsigned long endMillis = millis() + 1000;
-    while (millis() < endMillis)
-        Digits_step(len_mm);
+    Digits_step(len_cm);
 }
 
 
